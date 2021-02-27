6cd56fecf513e7d8eb43c57e4ae7f97405b069a5
Suraj-92 <suraj.temkar.92@gmail.com>
2021-02-27 09:49:31 +0530

    Cab Invoice Service

---
+++ src/test/java/com/InvoiceServiceTest.java
@@ -0,0 +1,34 @@
+package com;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+
+    @Test
+    public void givenDistanceAndTimeReturnTotalFare() {
+       InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+       double distance = 2.0;
+       int time = 5;
+       double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+    @Test
+    public void givenLessDistanceAndTimeReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRidesReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = { new Ride(2.0, 5),
+                new Ride(0.1, 1),
+                };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare,0.0);
+    }
+}
